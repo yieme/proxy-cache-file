babb55845c8bfa53daf629da3be5840bd5bb2f5d
yieme <yieme@cormea.com>
2015-05-22 14:17:23 -0600

    rework

--- index.js
+++ index.js
@@ -6,16 +6,22 @@
  *  @copyright  Copyright (C) 2015 by Yieme
  *  @module     proxyCacheFile
  */
-
-'use strict';
 var _              = require('lodash')
 var fs             = require('fs')
 var mkdirp         = require('mkdirp')
 var MD5            = require('MD5')
-var request        = require('request')
+var request        = require('superagent')
 var path           = require('path')
+var logger      = {
+  info:  function(msg) { console.log('info:', msg) },
+  debug: function(msg) { console.log('debug:', msg) },
+  warn:  function(msg) { console.warn('warn:', msg) },
+  error: function(msg) { console.error('error:', msg) },
+  log:   console.log,
+}
 var options        = {
-  dir:       './tmp', // falsely to disable file cache
+  dir:        './tmp', // falsely to disable file cache
+  logger:     logger,
 }
 var dirExists = { }
 
@@ -44,12 +50,21 @@ function proxyCacheFile(req, callback) {
   }
   req.dir = req.dir || options.dir
 
+  options.logger.debug('proxyCacheFile: ' + req.url)
+
   function cacheFile(filePath, headers, data) {
+    options.logger.debug('cacheFile: ' + req.url + ' to ' + filePath)
+
     function writeFile() {
-      var writeStream = fs.createWriteStream(filePath)
-      writeStream.write(data)
-      writeStream.end()
-      fs.writeFile(filePath + '.json', JSON.stringify(headers), 'utf8', function(err) {
+      if ('string' == typeof data) {
+        fs.writeFile(filePath, data, 'utf-8', function(err) {
+          if (err) return callback(err)
+        })
+      } else {
+        var writeStream = fs.createWriteStream(filePath)
+        data.pipe(writeStream)
+      }
+      fs.writeFile(filePath + '.json', JSON.stringify(headers), 'utf-8', function(err) {
         if (err) return callback(err)
       })
     }
@@ -66,7 +81,6 @@ function proxyCacheFile(req, callback) {
             dirExists[options.dir] = true
             writeFile()
           })
-          writeFile()
         }
       })
     }
@@ -77,52 +91,65 @@ function proxyCacheFile(req, callback) {
   }
 
   function tryProxy(filePath) {
-    var param = { method: 'GET', gzip: true, uri: req.url }
-    request(param, function (error, response, body) {
-      if (error) return callback(error)
+    options.logger.debug('tryProxy: ' + req.url)
+
+    request.head(req.url).end(function tryProxyHead(err, res) {
+      if (err) return callback(err)
       var headers   = {
-        type: response.headers['content-type']
+        code: res.status,
+        type: res.type
       }
       var result = { headers: headers }
-      if (response.statusCode == 200) {
-        if (filePath) cacheFile(filePath, headers, body)
-        result.body = body
-      } else {
-        result.headers.type = 'application/json'
-        result.headers.code = response.statusCode
-        result.body = errorBody(response.statusCode)
+      if (!res.ok) {
+        result.body = errorBody(res.status)
+        return callback(null, result)
       }
-      if (req.returnUrl) result.url = req.url
-      callback(null, result)
+      if (req.asStream) {
+        var stream = request.get(req.url)
+        cacheFile(filePath, headers, stream)
+        result.stream = stream
+        callback(null, result)
+      } else {
+        request.get(req.url).buffer().end(function (err, res) {
+          if (err) return callback(err)
+          cacheFile(filePath, headers, res.text)
+          result.body = res.text
+          callback(null, result)
+        })
+      }
     })
   }
 
   function tryFileCache(filePath) {
+    options.logger.debug('tryFileCache: ' + req.url + ' from ' + filePath)
+
     fs.exists(filePath, function(exists) {
       if (exists) {
-        fs.readFile(filePath + '.json', 'utf8', function(err, headers) {
+        fs.readFile(filePath + '.json', 'utf-8', function(err, headers) {
           if (err) return callback(err)
           headers = JSON.parse(headers)
           var result = { headers: headers, body: errorBody(headers.code), cache: 'file' }
           if (req.returnUrl) result.url = req.url
-          if (headers.code) return callback(null, result)
-          fs.readFile(filePath, 'utf8', function(err, body) {
-            if (err) return callback(err)
-            result.body = body
+          if (headers.code !== 200) return callback(null, result)
+          if (req.asStream) {
+            result.stream = fs.createReadStream(filePath)
             callback(null, result)
-          })
+          } else {
+            fs.readFile(filePath, 'utf-8', function(err, body) {
+              if (err) return callback(err)
+              result.body = body
+              callback(null, result)
+            })
+          }
         })
       } else {
         tryProxy(filePath)
       }
     })
   }
-  if (options.logRequest) {
-    var logger = (req.locals && req.locals._log) ? req.locals._log : console
-    logger.info('proxyCacheFile:', req.url)
-  }
-  if (!req.url)                   return callback(new ProxyCacheFileError('Missing req.url'))
-  if (req.url.indexOf('..') >= 0) return callback(new ProxyCacheFileError('Invalid req.url: ' + req.url))
+
+  if (!req.url)                   return callback('Missing req.url')
+  if (req.url.indexOf('..') >= 0) return callback('Invalid req.url: ' + req.url)
 	if (req.dir) {
     tryFileCache(path.normalize(req.dir + '/' + MD5(req.url)))
 	} else {
